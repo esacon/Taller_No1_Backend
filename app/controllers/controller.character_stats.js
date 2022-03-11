89826b02b53af591ca857f2952a9744ae29d9eea
Esaco <esaco@uninorte.edu.co>
2022-03-10 20:07:29 -0500

    Database filled out.

--- app/controllers/controller.character_stats.js
+++ app/controllers/controller.character_stats.js
@@ -1,9 +1,23 @@
-const data = require('../config/data')['character_stats']
+let data = require('../config/data')['character_stats']
+const genId = 0;
 
 module.exports = {
 
     async create(req, res) {
         const { nombre, apellido, cedula, rol, usuario, contraseña } = req.body;
+        const user = req.body;
+        if (user.Nombre && user.Username) {
+            const id = genId++;
+            const newUser = {
+                Username: user.Username,
+                Nombre: user.Nombre,
+                idUser: id
+            }
+            data = [...data, data];
+            res.status(200).json({ message: 'Success' });
+        } else {
+            res.status(400).end();
+        }
     },
 
     async getAll(req, res) {
@@ -12,6 +26,15 @@ module.exports = {
 
     async getID(req, res) {
         const id = parseInt(req.params.id);
+        const dataID = data.find(info => info.id === id);
+        if (dataID) {
+            console.log(data)
+            const info = data.filter(info => info.id === id);
+            console.log(info)
+            res.status(204).json();
+        } else {
+            res.status(404).end();
+        }
     }, 
 
     async update(req, res) {
@@ -20,6 +43,13 @@ module.exports = {
 
     async remove(req, res) {
         const id = parseInt(req.params.id);
+        const dataID = data.find(info => info.id === id);
+        if (dataID) {
+            data = data.filter(info => info.id !== id);
+            res.status(204).json({ message: 'Success' });
+        } else {
+            res.status(404).end();
+        }
     }
 
 
